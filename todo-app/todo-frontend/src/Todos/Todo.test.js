376b80ec788e0c625f0d3f7bc02e91db63bdd7da
Sami <samialeksi.tuomela@gmail.com>
2024-01-18 18:15:40 +0200

    added 12.14

--- todo-app/todo-frontend/src/Todos/Todo.test.js
+++ todo-app/todo-frontend/src/Todos/Todo.test.js
@@ -4,7 +4,7 @@ import React from "react"
 
 test("Todo has correct text", () => {
   const todo = renderer.create(
-    <Todo todo={{ text: "Some text on todo", done: false }} />
+      <Todo todo={{ text: "Some text on todo", done: false }} onClickComplete={() => console.log('yep')} onClickDelete={() => console.log('yep')} />
   )
 
   expect(JSON.stringify(todo)).toContain(
